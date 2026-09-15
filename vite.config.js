2fd5f29cbd1240c30d79a167cd76ad358cb7a575
kdlsystemofc-glitch <kdlsystemofc@gmail.com>
2026-09-15 09:43:57 -0300

    production-rc1: Final Preview Deployment Configuration & Touch Target Enhancement

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+export default {
+  server: {
+    host: true,
+    allowedHosts: true
+  },
+  preview: {
+    host: true,
+    port: 5190,
+    allowedHosts: true,
+    headers: {
+      'X-Robots-Tag': 'noindex, nofollow, noarchive'
+    }
+  }
+};
